089693b06d3ab6a0db501a28a06158c4c16b85f6
michallef <142462683+michallef@users.noreply.github.com>
2023-08-23 18:50:03 -0700

    Create main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
     males = (int(input("Enter the number of males in the class: ")))
     females = (int(input("Enter the number of females in the class: ")))
-    total = males + females
-    m_perc = males / 1
-    f_perc = females / 1.0
-    print (f'The total number of students:       {total}')
-    print (f'The number of males and females:     {males} \t {females}')
-    print (f'The percentage of males and females: {m_perc:.2f}% \t {f_perc:.2f}%')
+    total = (males + females)
+    m_perc = males / total * 100
+    f_perc = females / total * 100
+    print (f'The total number of students:   \t    {total}')
+    print (f'The number of males and females: \t    {males} \t         {females}')
+    print (f'The percentage of males and females: \t    {m_perc:.2f}% \t {f_perc:.2f}%')
     
 
     """
